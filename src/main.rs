10f31b74f8fd6163a8c301ab9d428cbe43b2e1b6
Matteo <46228228+RampeoMattone@users.noreply.github.com>
2021-01-11 23:49:27 +0100

    stuffff

--- src/main.rs
+++ src/main.rs
@@ -29,11 +29,12 @@ use serenity::{
     },
     Result as SerenityResult,
 };
-use serenity::model::prelude::GuildId;
+use serenity::model::prelude::{GuildId, UserId};
 use serenity::prelude::TypeMapKey;
 use songbird::{
-    CoreEvent,
     driver::{Config as DriverConfig, DecodeMode},
+    model::payload::{Speaking},
+    CoreEvent,
     Event,
     EventContext,
     EventHandler as VoiceEventHandler,
@@ -86,21 +87,21 @@ impl Buffer {
 }
 
 struct Receiver {
-    buffer: Arc<Mutex<HashMap<u32, Buffer>>>,
+    lobby: Arc<(Mutex<HashMap<u32, Buffer>>, Mutex<HashMap<u32, UserId>>)>,
 }
 
 impl Receiver {
-    pub fn new(buffer: Arc<Mutex<HashMap<u32, Buffer>>>) -> Self {
+    pub fn new(lobby: Arc<(Mutex<HashMap<u32, Buffer>>, Mutex<HashMap<u32, UserId>>)>) -> Self {
         // You can manage state here, such as a buffer of audio packet bytes so
         // you can later store them in intervals.
-        Self { buffer }
+        Self { lobby }
     }
 }
 
-struct AudioBuffers; // void struct used to generate a typemap that holds all active lobbies
+struct Lobbies; // void struct used to generate a typemap that holds all active lobbies
 
-impl TypeMapKey for AudioBuffers {
-    type Value = Arc<RwLock<HashMap<GuildId, Arc<Mutex<HashMap<u32, Buffer>>>>>>; // a game is held within a lobby. the text channel id is the lobby's unique code
+impl TypeMapKey for Lobbies {
+    type Value = Arc<RwLock<HashMap<GuildId, Arc<(Mutex<HashMap<u32, Buffer>>, Mutex<HashMap<u32, UserId>>)>>>>; // a game is held within a lobby. the text channel id is the lobby's unique code
 }
 
 #[async_trait]
@@ -113,7 +114,7 @@ impl VoiceEventHandler for Receiver {
                 // An event which fires for every received audio packet,
                 // containing the decoded data.
                 if let Some(audio) = audio {
-                    let mut buffer = self.buffer.lock().unwrap();
+                    let buffer = &mut self.lobby.0.lock().unwrap();
                     if let Some(buffer) = buffer.get_mut(&packet.ssrc) {
                         buffer.push(audio);
                     } else {
@@ -121,6 +122,17 @@ impl VoiceEventHandler for Receiver {
                         new_buffer.push(audio);
                         buffer.insert(packet.ssrc, new_buffer);
                     }
+                }
+            },
+            Ctx::SpeakingStateUpdate(
+                Speaking {speaking, ssrc, user_id, ..}
+            ) => {
+                // You can implement your own logic here to handle a user who has joined the
+                // voice channel e.g., allocate structures, map their SSRC to User ID.
+                let buffer = &mut self.lobby.1.lock().unwrap();
+                if let Some(user_id) = user_id {
+                    let id = user_id.0;
+                    buffer.insert(*ssrc, UserId(id));
                 }
             },
             _ => {
@@ -176,7 +188,7 @@ async fn main() {
 
     {
         let mut data = client.data.write().await;
-        data.insert::<AudioBuffers>(Arc::new(RwLock::new(HashMap::default())));
+        data.insert::<Lobbies>(Arc::new(RwLock::new(HashMap::default())));
     }
 
     let _ = client.start().await.map_err(|why| println!("Client ended: {:?}", why));
@@ -213,16 +225,22 @@ async fn join(ctx: &Context, msg: &Message) -> CommandResult {
         let mut handler = handler_lock.lock().await;
 
         let audio_buffer: HashMap<u32, Buffer> = HashMap::new();
-        let audio_buffer = Arc::new(Mutex::new(audio_buffer));
+        let ssrc_map: HashMap<u32, UserId> = HashMap::new();
+        let lobby = Arc::new((Mutex::new(audio_buffer), Mutex::new(ssrc_map)));
         {
             let data_write = ctx.data.write().await;
-            let buffers_lock = data_write.get::<AudioBuffers>().expect("Typemap incomplete").clone();
-            buffers_lock.write().await.insert(guild_id, audio_buffer.clone());
+            let buffers_lock = data_write.get::<Lobbies>().expect("Typemap incomplete").clone();
+            buffers_lock.write().await.insert(guild_id, lobby.clone());
         }
 
         handler.add_global_event(
             CoreEvent::VoicePacket.into(),
-            Receiver::new(audio_buffer.clone()),
+            Receiver::new(lobby.clone()),
+        );
+
+        handler.add_global_event(
+            CoreEvent::SpeakingStateUpdate.into(),
+            Receiver::new(lobby.clone()),
         );
 
         check_msg(msg.channel_id.say(&ctx.http, &format!("Joined {}", channel_id.mention())).await);
@@ -262,6 +280,7 @@ async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
 #[only_in(guilds)]
 async fn dump(ctx: &Context, msg: &Message) -> CommandResult {
     let guild = msg.guild(&ctx.cache).await.expect("could not find guild from message");
+    let members = guild.members;
     let guild_id = guild.id;
     let directory = &format!(".temp_audio/{}", guild_id)[..];
     if let Err(why) = std::fs::create_dir_all(directory) {
@@ -270,11 +289,13 @@ async fn dump(ctx: &Context, msg: &Message) -> CommandResult {
         let mut paths = Vec::new();
         {
             let data_read = ctx.data.read().await;
-            let buffers_lock = data_read.get::<AudioBuffers>().expect("Typemap incomplete").clone();
-            let buffer_lock = buffers_lock.read().await.get(&guild_id).expect("could not acquire a read lock on the data").clone();
-            let mut buffer = buffer_lock.lock().expect("failed to get a write lock for buffer");
-            for (id, buffer) in buffer.drain() {
-                let path = format!("{}/{}.wav",directory, id);
+            let lobbies_lock = data_read.get::<Lobbies>().expect("Typemap incomplete").clone();
+            let lobby_lock = lobbies_lock.read().await.get(&guild_id).expect("could not acquire a read lock on the data").clone();
+            let mut lobby = lobby_lock.0.lock().unwrap();
+            let ssrc_map = lobby_lock.1.lock().unwrap();
+            for (id, buffer) in lobby.drain() {
+                let name = &members.get(ssrc_map.get(&id).expect("ssrc not in map")).unwrap().user.name;
+                let path = format!("{}/{}.wav",directory, *name);
                 let mut writer = hound::WavWriter::create(&path, SPEC).unwrap();
                 for sample in buffer.pop().iter() {
                     writer.write_sample(*sample)?;
@@ -298,12 +319,13 @@ async fn dump(ctx: &Context, msg: &Message) -> CommandResult {
 #[only_in(guilds)]
 async fn clean(ctx: &Context, msg: &Message) -> CommandResult {
     let guild = msg.guild(&ctx.cache).await.expect("could not find guild from message");
+
     let guild_id = guild.id;
     {
         let data_read = ctx.data.read().await;
-        let buffers_lock = data_read.get::<AudioBuffers>().expect("Typemap incomplete").clone();
-        let buffer_lock = buffers_lock.read().await.get(&guild_id).expect("could not acquire a read lock on the data").clone();
-        let mut buffer = buffer_lock.lock().expect("failed to get a write lock for buffer");
+        let lobbies_lock = data_read.get::<Lobbies>().expect("Typemap incomplete").clone();
+        let lobby_lock = lobbies_lock.read().await.get(&guild_id).expect("could not acquire a read lock on the data").clone();
+        let buffer = &mut lobby_lock.0.lock().expect("could not acquire a read lock on the data");
         buffer.clear();
     }
     check_msg(msg.channel_id.send_message(ctx, |m| m.content("The buffer has been cleared. No need to thank me")).await);
